3e451035ee58a331a4f9acf0e280f7bc48c9cda7
Sitharth R <rsitharth77@gmail.com>
2026-09-05 15:16:38 +0530

    Time: 4 ms (88.55%), Space: 133.4 MB (14.89%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,12 +1,7 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-        int[] prefix = new int[n];
-        prefix[0] = nums[0];
 
-        for(int i  = 1; i < n; i++){
-            prefix[i] = Math.max(prefix[i - 1] , nums[i]); 
-        }
 
         int[] prefix2 = new int[n];
         prefix2[n-1] = nums[n-1];
@@ -14,15 +9,14 @@ class Solution {
         for(int i  = n - 2; i >= 0; i--){
             prefix2[i] = Math.min(prefix2[i + 1] , nums[i]); 
         }
-        System.out.print(Arrays.toString(prefix));
-
-        int j = 0;
-        int minn = Integer.MAX_VALUE;
-        for(int i = 0; i < n; i++){
-            if(prefix[i] - prefix2[i] <= k ){
+        int maxx = 0;
+        for(int i = 0; i < n; i ++){
+            maxx = Math.max(maxx, nums[i]);
+            if(maxx - prefix2[i] <= k ){
                 return i;
             }
         }
+
         return -1;
     }
 }
