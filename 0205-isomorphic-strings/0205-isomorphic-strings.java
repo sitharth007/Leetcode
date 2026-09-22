cb62a2494c644e05f1d42c1f0124051ccd318cb3
Sitharth R <rsitharth77@gmail.com>
2026-09-22 14:24:16 +0530

    Time: 20 ms (23.64%), Space: 44.3 MB (9.71%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> map = new HashMap<>();
+
+        if(s.length() != t.length()) return false;
+
+        for(int i = 0;i<s.length();i++){
+            if(!map.containsKey(s.charAt(i)) && map.containsValue(t.charAt(i))) return false;
+            else if(map.containsKey(s.charAt(i)) && map.get(s.charAt(i)) != t.charAt(i)) return false;
+            else{
+                map.put(s.charAt(i) , t.charAt(i));
+            }
+        }
+        return true;
+    }
+}
